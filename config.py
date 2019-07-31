c526b859288ddc3a56a92c990302b9b568cd1e9c
Zainul Abid <48980248+zainulhibath@users.noreply.github.com>
2019-07-31 13:37:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from sample_config import Config 
 
 class Development(Config): 
-APP_ID = 851576  
+APP_ID = "851576"  
 API_HASH = "c8c8ee4adf8f441bdbb71d5ff9f6bff6" 
-TG_BOT_TOKEN_BF_HER = "971554791:AAHr44e4nYflq9KCuhmAiyGovcjVnYUhZTE" 
-TG_BOT_USER_NAME_BF_HER = "@Zainstechgroup_bot" 
+TG_BOT_TOKEN_BF_HER = "" 
+TG_BOT_USER_NAME_BF_HER = "" 
 UB_BLACK_LIST_CHAT = [] # chat ids or usernames, it is recommended to use chat ids, 
 # providing usernames means an additional overhead for the user 
 CHATS_TO_MONITOR_FOR_ANTI_FLOOD = [] 
